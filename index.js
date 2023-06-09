f995ac0bc7b2ef6353f2042e6e885f7ea49fc393
Rafael Palma Almeida <rafaelpalmaalmeida@hotmail.com>
2023-06-09 10:58:04 -0300

    Entrega aula 03

---
+++ index.js
@@ -0,0 +1,5 @@
+let inseraNumero = parseInt(prompt("Inserir Número"))
+for (let i = 1; i <= 10; i++) {
+    let resultado = inseraNumero * i ;
+    alert(inseraNumero +" X "+ i +" = "+ resultado);
+}
